683eb6e9332b7708d2a6dac2869edb18c1a33d3c
bandrewfisher <bf6038@gmail.com>
2018-08-23 15:12:44 -0600

    Updated database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost", "root", "snowball", "brettipedia");
+	$conn = new mysqli("localhost", "root", "password", "mydatabase");
 	
 	// Check connection
 	if ($conn->connect_error) {
